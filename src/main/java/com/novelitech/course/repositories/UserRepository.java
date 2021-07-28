baa84f09e56f9780e4f09f0b1ec9103b9c533d0c
Bruno Noveli <brunimzuppwolf@gmail.com>
2021-07-28 17:08:46 -0300

    Camada de serviço, registro de componentes

--- src/main/java/com/novelitech/course/repositories/UserRepository.java
+++ src/main/java/com/novelitech/course/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.novelitech.course.repositories;
 
 import com.novelitech.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // O Spring Data JPA já tem uma implementação padrão para essa interface, então eu não preciso fazer
 // uma implementação por mim mesmo
+//@Repository - Não é necessário essa anotação pois JpaRepository já possui esse registro como um componente do Spring
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/novelitech/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.novelitech.course.services;
+
+import com.novelitech.course.entities.User;
+import com.novelitech.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+//@Component
+@Service // O de cima funciona, mas coloca esse pra ficar mais semanticamente correto. Funciona igual o AddScoped do .NET
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
