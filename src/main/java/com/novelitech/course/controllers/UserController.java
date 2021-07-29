27aa94769ede0f02b42657a22e2ccf158250ebf8
Bruno Noveli <brunimzuppwolf@gmail.com>
2021-07-29 17:42:59 -0300

    Deleção de User

--- src/main/java/com/novelitech/course/controllers/UserController.java
+++ src/main/java/com/novelitech/course/controllers/UserController.java
@@ -40,4 +40,10 @@ public class UserController {
 
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
