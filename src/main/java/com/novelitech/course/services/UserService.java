92052dbb7fc9dab1638dd54e6989b057d6a01192
Bruno Noveli <brunimzuppwolf@gmail.com>
2021-07-29 23:26:28 -0300

    Tratamento de exceção - delete

--- src/main/java/com/novelitech/course/services/UserService.java
+++ src/main/java/com/novelitech/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.novelitech.course.services;
 
 import com.novelitech.course.entities.User;
 import com.novelitech.course.repositories.UserRepository;
+import com.novelitech.course.services.exceptions.DatabaseException;
 import com.novelitech.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -32,7 +35,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch(EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch(DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
---
+++ src/main/java/com/novelitech/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.novelitech.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+}
